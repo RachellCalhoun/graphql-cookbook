b11e00ac3b3a1bafe81e0e4d04811f741ea21d21
Rachell Calhoun <rachell.calhoun@gmail.com>
2018-10-14 17:57:49 -0400

    step 3 add recipe to recipe query

--- recipes/schema/recipes.py
+++ recipes/schema/recipes.py
@@ -9,7 +9,15 @@ class RecipeNode(DjangoObjectType):
         model = Recipe
 
 class RecipeQuery(object):
+    recipe = graphene.Field(Recipenode, id=graphene.Int(required=True))
     all_recipes = graphene.List(RecipeNode)
+
+    def resolve_recipe(self, info, **kwargs):
+        id = kwargs.get('id')
+        try:
+            return Recipe.objects.get(pk=id)
+        except (Recipe.DoesNotExist):
+            return None
 
     def resolve_all_recipes(self, info, **kwargs):
         return Recipe.objects.all()
